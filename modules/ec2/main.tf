b09cf4c1b89981793c68e921d7c6aead3ed5ee27
Yuji Tsunomori <yuji.tsunomori@gmail.com>
2021-12-23 15:12:33 +0900

    Temporary add provisioning

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "appserver" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.security_group_id]
   subnet_id              = var.subnet_id
+  key_name               = var.key_name
 
   root_block_device {
     volume_type = "gp2"
@@ -15,9 +16,23 @@ resource "aws_instance" "appserver" {
   tags = {
     Name = "${var.app_name}-${var.app_version}-${var.environment}-appserver"
   }
+
+  user_data = <<EOF
+#!/bin/bash
+sudo apt -y update
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo apt -y install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+EOF
 }
 
-# ------------------------------
+# ------------------------------a
 # Elastic IP
 # ------------------------------
 resource "aws_eip" "appserver" {
